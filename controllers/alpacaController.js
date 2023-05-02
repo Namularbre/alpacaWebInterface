0ddda184513e681aa1060f7953daeb5ec668f14b
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-05-02 21:53:26 +0200

    rollback last commit

--- controllers/alpacaController.js
+++ controllers/alpacaController.js
@@ -1,15 +1,8 @@
 const pty = require('node-pty');
 
 class AlpacaController {
-    static #processAlreadyRunning = false;
 
     static async getResponse(req, res) {
-        const apologiesPhrase = "Désolé, je suis très occupé à répondre au question d'une autre personne. Pouvez-vous patienter un peu ?";
-        if (AlpacaController.#processAlreadyRunning) {
-            res.status(200).send({response : apologiesPhrase});
-            return;
-        }
-
         const question = req.query.question;
         let alpacaProcess = pty.spawn(`C:\\alpaca\\alpaca.cpp\\Release\\web.bat`, [`${question}`], {
             name: 'xterm-color',
@@ -18,8 +11,6 @@ class AlpacaController {
             cwd: '',
             env: process.env
         });
-
-        AlpacaController.#processAlreadyRunning = true;
 
         let response = "";
 
@@ -32,8 +23,6 @@ class AlpacaController {
             alpacaProcess.onExit(() => {
                 response = response.split('repeat_last_')[1];
                 response = response.split('[end of text]')[0];
-
-                AlpacaController.#processAlreadyRunning = false;
                 res.send({response : response});
             });
         } else {
