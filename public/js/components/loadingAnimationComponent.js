38da0f1a0c6f74c895f08180f9af43c4582cefb6
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-05-08 17:34:07 +0200

    added a loading animation

--- public/js/components/loadingAnimationComponent.js
+++ public/js/components/loadingAnimationComponent.js
@@ -9,8 +9,8 @@ class LoadingAnimationComponent extends HTMLElement {
 
     render() {
         this.innerHTML = `
-            <div class="mx-auto position-fixed bottom-50 start-50 translate-middle-x translate-middle-y z-index-2">
-                <p>Travail en cours...</p>
+            <div class="spinner-grow mx-auto position-fixed bottom-50 start-50 translate-middle-x translate-middle-y z-index-2">
+                <span class="sr-only m-3" role="status"></span>
             </div>
         `;
     }
